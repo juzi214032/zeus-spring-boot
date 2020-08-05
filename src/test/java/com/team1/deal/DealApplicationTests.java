4ce6eda8d1769d62c59090c875e6d0feb6a9bde5
Zhang Shufan <412999892@qq.com>
2020-08-05 14:57:48 +0800

    init

---
+++ src/test/java/com/team1/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
