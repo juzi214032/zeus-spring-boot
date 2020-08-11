443c94d4572dd0796c7a3ed0b02de03d5974b6e3
Zhang Shufan <412999892@qq.com>
2020-08-11 14:58:28 +0800

    fix:修改国电用户查看采购详情的用户id获取方式

--- src/main/java/team1/deal/controller/quotedPrice/QuotedPriceInfoController.java
+++ src/main/java/team1/deal/controller/quotedPrice/QuotedPriceInfoController.java
@@ -7,13 +7,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import team1.deal.model.dto.PageParamDTO;
+import team1.deal.model.po.UserPO;
 import team1.deal.model.vo.DemandOrderInfoVO;
 import team1.deal.model.vo.ResponseVO;
 import team1.deal.service.DemandOrderService;
 import team1.deal.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +32,9 @@ public class QuotedPriceInfoController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate<Object, UserPO> redisTemplate;
+
     @ApiOperation("采购信息列表")
     @GetMapping("/demandOrderList")
     public ResponseVO<Page<DemandOrderInfoVO>> demandOrderList(@RequestBody(required = false) PageParamDTO pageParamDTO){
@@ -38,11 +44,11 @@ public class QuotedPriceInfoController {
 
     @ApiOperation("国电用户查看采购信息详情")
     @GetMapping("/demandOrderInfo/company/{orderId}")
-    public ResponseVO<Map<String,Object>> demandOrderInfoCompany(@ApiParam("采购订单id") @PathVariable Integer orderId){
+    public ResponseVO<Map<String,Object>> demandOrderInfoCompany(@ApiParam("采购订单id") @PathVariable Integer orderId, HttpServletRequest httpServletRequest){
         Map map = new HashMap();
         map.put("demandOrder",demandOrderService.getDemandOrderById(orderId));
-        //模拟用户id，后头可用token替换
-        map.put("quotedPriceInfo",userService.selectSupplier(1));
+        UserPO userPO = redisTemplate.opsForValue().get(httpServletRequest.getHeader("Token"));
+        map.put("quotedPriceInfo",userService.selectSupplier(userPO.getId()));
         return  new ResponseVO<>(map);
     }
 
