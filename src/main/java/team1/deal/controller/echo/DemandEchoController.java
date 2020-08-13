f44554831cb9111cb65310a7580ed236d6de375a
Zhang Shufan <412999892@qq.com>
2020-08-13 15:03:30 +0800

    fix:修改需求订单信息回显为详细和简要

--- src/main/java/team1/deal/controller/echo/DemandEchoController.java
+++ src/main/java/team1/deal/controller/echo/DemandEchoController.java
@@ -2,15 +2,16 @@ package team1.deal.controller.echo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team1.deal.model.po.SaveDemandOrderPO;
-import team1.deal.model.po.SaveQuotedPriceInfoPO;
 import team1.deal.model.po.UserPO;
 import team1.deal.model.vo.ResponseVO;
+import team1.deal.service.DemandOrderService;
 import team1.deal.service.EchoService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,16 +27,28 @@ public class DemandEchoController {
     @Autowired
     private RedisTemplate<Object, UserPO> redisTemplate;
 
+    @Autowired
+    private DemandOrderService demandOrderService;
+
     /**
-     * 需求订单回显
+     * 需求订单简要信息回显
      * @param httpServletRequest
      * @return
      */
-    @ApiOperation("需求订单回显")
-    @PostMapping(value = "/DemandEcho")
-    public ResponseVO DemandEcho(HttpServletRequest httpServletRequest){
+    @ApiOperation("需求订单简要信息回显")
+    @PostMapping(value = "/DemandEchoBrief")
+    public ResponseVO DemandEchoBrief(HttpServletRequest httpServletRequest){
         UserPO userPO = redisTemplate.opsForValue().get(httpServletRequest.getHeader("Token"));
-        return new ResponseVO(echoService.getToAudit(userPO.getId()));
+        return new ResponseVO(echoService.getDemandOrderBriefInfo(2));
+    }
+
+    /**
+     * 需求订单详细信息回显
+     */
+    @ApiOperation("需求订单详细信息回显")
+    @PostMapping(value = "/DemandEchoDetail/{orderId}")
+    public ResponseVO DemandEchoDetail(@ApiParam("订单id") @PathVariable Integer orderId){
+        return new ResponseVO(demandOrderService.getDemandOrderById(orderId));
     }
 
 
---
+++ src/main/java/team1/deal/model/vo/DemandOrderBriefInfoVO.java
@@ -0,0 +1,36 @@
+package team1.deal.model.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@ApiModel("需求订单简要信息")
+public class DemandOrderBriefInfoVO {
+    @ApiModelProperty(value = "需求订单id")
+    private Integer id;
+
+    @ApiModelProperty(value = "状态代码（0：待审核状态，1：电厂一级审核通过状态，2：电厂二级审核通过状态，3：子公司一级审核通过状态，4：子公司二级审核通过状态，5完成状态，-1：待修改状态,-2：未创建状态")
+    private Integer status;
+
+    @ApiModelProperty(value = "煤炭种类")
+    @TableField("coalType")
+    private String coalType;
+
+    @ApiModelProperty(value = "采购数量")
+    @TableField("coalNumber")
+    private Integer coalNumber;
+
+    @ApiModelProperty(value = "交货日期")
+    @TableField("deliveryTime")
+    private LocalDateTime deliveryTime;
+
+    @ApiModelProperty(value = "报价截止时间")
+    @TableField("lastTime")
+    private LocalDateTime lastTime;
+}
